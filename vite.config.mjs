07af2391f951e329b98799195a858e8602d1b753
Marcos Franciosi <marcosfranciosi@live.com>
2026-01-29 13:00:11 -0300

    add pwa

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -20,6 +21,7 @@ export default defineConfig({
     Vue({
       template: { transformAssetUrls },
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify({
       autoImport: true,
